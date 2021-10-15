2b1e912ba948537030d35167180a495ed0b9c91d
Fastroof <lytvdan@gmail.com>
2021-10-15 18:44:35 -0400

    Practice 2

--- src/main/java/ua/edu/sumdu/j2se/lytvynenko/tasks/Main.java
+++ src/main/java/ua/edu/sumdu/j2se/lytvynenko/tasks/Main.java
@@ -2,7 +2,20 @@ package ua.edu.sumdu.j2se.lytvynenko.tasks;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+    public static void main(String[] args) {
+        Task task = new Task("Зустріч у кафе", 10);
+        System.out.println(task.getTitle());
+        System.out.println(task.getTime());
+        System.out.println(task.isActive());
+        System.out.println(task.isRepeated());
+
+        Task repeatedTask = new Task("Ранкова пробіжка", 8, 200, 24);
+        System.out.println(repeatedTask.getTitle());
+        System.out.println(repeatedTask.getStartTime());
+        System.out.println(repeatedTask.getEndTime());
+        System.out.println(repeatedTask.getRepeatInterval());
+        System.out.println(repeatedTask.isActive());
+        System.out.println(repeatedTask.isRepeated());
+        repeatedTask.getTime();
+    }
 }
---
+++ src/main/java/ua/edu/sumdu/j2se/lytvynenko/tasks/Task.java
@@ -0,0 +1,152 @@
+package ua.edu.sumdu.j2se.lytvynenko.tasks;
+
+public class Task {
+
+    private String title;
+    private int time;
+    private int start;
+    private int end;
+    private int interval;
+    private boolean active;
+    private boolean repeat;
+
+    /**
+     * A constructor that constructs an inactive task
+     * that runs at a specified time without repetition, with a given name.
+     *
+     * @param title task name
+     * @param time task execution time
+     */
+    public Task(String title, int time) {
+        this.title = title;
+        this.time = time;
+        this.active = false;
+        this.repeat = false;
+    }
+
+
+    /**
+     * A constructor that constructs an inactive task
+     * that runs at a specified time (since start time to end time)
+     * at a specified interval and has a specified name.
+     *
+     * @param title task name
+     * @param start start time
+     * @param end end time
+     * @param interval repeat interval
+     */
+    public Task(String title, int start, int end, int interval) {
+        this.title = title;
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+        this.active = false;
+        this.repeat = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    /**
+     * Get the execution time of a task that is not repeated.
+     * If the task is repeated, the method returns the start time of the repetition.
+     */
+    public int getTime() {
+        return (this.repeat ? start : time);
+    }
+
+    /**
+     * Set the execution time of a task that is not repeated.
+     * If the task is repeated, it becomes non-repetitive.
+     */
+    public void setTime(int time) {
+        this.time = time;
+        if (this.repeat) {
+            this.repeat = false;
+        }
+    }
+
+    /**
+     * Get the start time of a task that is repeated.
+     * If the task is not repeated, the method returns the execution time.
+     */
+    public int getStartTime() {
+        return (this.repeat ? start : time);
+    }
+
+    /**
+     * Get the end time of a task that is repeated.
+     * If the task is not repeated, the method returns the execution time.
+     */
+    public int getEndTime() {
+        return (this.repeat ? end : time);
+    }
+
+    /**
+     * Get the repeat interval time of a task that is repeated.
+     * If the task is not repeated, the method returns 0.
+     */
+    public int getRepeatInterval() {
+        return (this.repeat ? interval : 0);
+    }
+
+    /**
+     * Set the start, end, repeat interval time of a task that is repeated.
+     * If the task is not repeated, it becomes repeated.
+     */
+    public void setTime(int start, int end, int interval) {
+        this.start = start;
+        this.end = end;
+        this.interval = interval;
+        if (!this.repeat) {
+            this.repeat = true;
+        }
+    }
+
+    public boolean isRepeated() {
+        return repeat;
+    }
+
+    /**
+     * Return the time of the next execution of the task after the current time.
+     * If after the specified time the task isn't executed, the method return -1.
+     */
+    public int nextTimeAfter(int current) {
+        int nextTimeAfter = -1;
+
+        if (active) {
+            if (repeat) {
+                if (current < start) {
+                    nextTimeAfter = start;
+                } else if (current < end) {
+                    for (int i = start + interval; i < end; i += interval) {
+                        if (current < i) {
+                            nextTimeAfter = i;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                if (current < time) {
+                    nextTimeAfter = time;
+                }
+            }
+        }
+
+        return nextTimeAfter;
+    }
+
+}
